1e138a6d555d98c9b7ae09fb8dd24bb85d55d219
Pro <pro@Macbook-pro-de-pro.local>
2022-10-21 20:37:50 +0200

    e

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,23 +1,38 @@
 #!/usr/bin/python3
+
 """
-Uses https://jsonplaceholder.typicode.com along with an employee ID to
-return information about the employee's todo list progress
+Module
 """
 
 import requests
-from sys import argv
+import sys
 
-if __name__ == '__main__':
-    userId = argv[1]
-    user = requests.get("https://jsonplaceholder.typicode.com/users/{}".
-                        format(userId), verify=False).json()
-    todo = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
-                        format(userId), verify=False).json()
-    completed_tasks = []
-    for task in todo:
-        if task.get('completed') is True:
-            completed_tasks.append(task.get('title'))
-    print("Employee {} is done with tasks({}/{}):".
-          format(user.get('name'), len(completed_tasks), len(todo)))
-    print("\n".join("\t {}".format(task) for task in completed_tasks))
-    
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(
+        sys.argv[1])
+    url_2 = "https://jsonplaceholder.typicode.com/users/{}/".format(
+        sys.argv[1])
+
+    response = requests.get(url)
+    result = response.json()
+    response_2 = requests.get(url_2)
+    result_2 = response_2.json()
+
+    item_2 = result_2.get('name')
+
+    count = 0
+    count_2 = 0
+
+    for item in result:
+        if item.get('userId') == int(sys.argv[1]):
+            count_2 += 1
+        if item.get('completed') and item.get('userId') == int(sys.argv[1]):
+            count += 1
+    print('Employee {} is done with tasks({}/{}):'.format(item_2,
+                                                          count, count_2))
+
+    for item in result:
+        if item.get('completed') and item.get('userId') == int(sys.argv[1]):
+            print("\t {}".format(item['title']))
+            
--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,30 +1,43 @@
 #!/usr/bin/python3
-"""
-Uses https://jsonplaceholder.typicode.com to return information about all
-employee's todo list progress
-"""
+"""Module"""
 
 import json
 import requests
 
+
+def get_employee_task(employee_id):
+    """Doc"""
+    user_url = "https://jsonplaceholder.typicode.com/users/{}" \
+        .format(employee_id)
+
+    user_info = requests.request('GET', user_url).json()
+
+    employee_username = user_info["username"]
+    todos_url = "https://jsonplaceholder.typicode.com/users/{}/todos/" \
+        .format(employee_id)
+    todos_info = requests.request('GET', todos_url).json()
+    return [
+        dict(zip(["task", "completed", "username"],
+                 [task["title"], task["completed"], employee_username]))
+        for task in todos_info]
+
+
+def get_employee_ids():
+    """Doc"""
+    users_url = "https://jsonplaceholder.typicode.com/users/"
+
+    users_info = requests.request('GET', users_url).json()
+    ids = list(map(lambda user: user["id"], users_info))
+    return ids
+
+
 if __name__ == '__main__':
-    users = requests.get("https://jsonplaceholder.typicode.com/users",
-                         verify=False).json()
-    userdict = {}
-    usernamedict = {}
-    for user in users:
-        uid = user.get("id")
-        userdict[uid] = []
-        usernamedict[uid] = user.get("username")
-    todo = requests.get("https://jsonplaceholder.typicode.com/todos",
-                        verify=False).json()
-    for task in todo:
-        taskdict = {}
-        uid = task.get("userId")
-        taskdict["task"] = task.get('title')
-        taskdict["completed"] = task.get('completed')
-        taskdict["username"] = usernamedict.get(uid)
-        userdict.get(uid).append(taskdict)
-    with open("todo_all_employees.json", 'w') as jsonfile:
-        json.dump(userdict, jsonfile)
+
+    employee_ids = get_employee_ids()
+
+    with open('todo_all_employees.json', "w") as file:
+        all_users = {}
+        for employee_id in employee_ids:
+            all_users[str(employee_id)] = get_employee_task(employee_id)
+        file.write(json.dumps(all_users))
         
